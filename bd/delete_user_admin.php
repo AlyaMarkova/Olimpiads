c8d23a83e46d5e579bb8aeb66fef9f3250fe6e9f
AlyaMarkova <alya.markova.3@gmail.com>
2017-03-11 01:17:46 +1000

    Рассылка при удалении профиля
    Рассылка приходит тому, чей профиль удалили

--- bd/delete_user_admin.php
+++ bd/delete_user_admin.php
@@ -1,22 +1,33 @@
 <?php
-	include ("../bd.php");
-	$data = json_decode($_POST['jsonData']);
-	$id=$data->id;
-	$id2=$data->id2;
-	$result = mysql_query("SELECT * FROM users WHERE id='$id'");
-	$myrow= mysql_fetch_array($result);
-	$rights=$myrow['rights'];
-	mysql_query("DELETE FROM users WHERE  id='$id'");
-	if($rights==1){
-		mysql_query("DELETE FROM comments WHERE user='$id' or reply='$id'");
-		mysql_query("DELETE FROM schoolboy_past_olympics WHERE schoolboy_users_id='$id'");
-		mysql_query("DELETE FROM schoolboy_olympics WHERE schoolboy_users_id='$id'");
-		mysql_query("DELETE FROM schoolboy WHERE Users_id='$id'");
-	}
-	if($rights==2){
-		mysql_query("DELETE FROM comments WHERE user='$id' or reply='$id'");
-		mysql_query("DELETE FROM professor WHERE  users_id='$id'");
-		mysql_query ("UPDATE  olympics SET professor_users_id = '$id2' WHERE professor_users_id='$id'");	
-	}
+include ("../bd.php");
+$data = json_decode($_POST['jsonData']);
+$id=$data->id;
+$id2=$data->id2;
+$myrow = mysql_fetch_array(mysql_query("SELECT * FROM users WHERE id='$id'"));
+
+/******** отсылаем сообщение о такой досадной новости *********/
+if ($myrow['rights'] == 1) 
+	$row = mysql_fetch_array(mysql_query("SELECT email FROM schoolboy WHERE Users_id='$id'"));
+else $row = mysql_fetch_array(mysql_query("SELECT email FROM professor WHERE users_id='$id'"));
+$email = $row['email'];
+$message = "Уважаемый ".$myrow['login']."!\nСообщаем, что Ваш профиль был удалён Администрацией сайта.\n\nС уважением,\nАдминистрация olimpiada.ru";
+mail($email, "Удаление профиля", $message, "Content-type:text/plane;    Charset=UTF-8\r\n");
+
+/***********************************************/
+
+//удаляем из бд
+$rights=$myrow['rights'];
+mysql_query("DELETE FROM users WHERE  id='$id'");
+if($rights==1){
+	mysql_query("DELETE FROM comments WHERE user='$id' or reply='$id'");
+	mysql_query("DELETE FROM schoolboy_past_olympics WHERE schoolboy_users_id='$id'");
+	mysql_query("DELETE FROM schoolboy_olympics WHERE schoolboy_users_id='$id'");
+	mysql_query("DELETE FROM schoolboy WHERE Users_id='$id'");
+}
+if($rights==2){
+	mysql_query("DELETE FROM comments WHERE user='$id' or reply='$id'");
+	mysql_query("DELETE FROM professor WHERE  users_id='$id'");
+	mysql_query ("UPDATE  olympics SET professor_users_id = '$id2' WHERE professor_users_id='$id'");	
+}
 	
 ?>

ca705a92c648fe8683973d1b8d5bae654ff4028e
AlyaMarkova <alya.markova.3@gmail.com>
2017-03-18 03:34:35 +1000

    Первые попытки сохранения в БД
    В форме добавлена проверка на кол-во дат и мест (они должны быть равны)
    
    Проблемы при сохранении:
    1. Не сохраняет место
    2. Не сохраняет и не апдейтит значение поля "stages"  у родительской
    олимпиады

--- bd/create_olimpiada.php
+++ bd/create_olimpiada.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['name_olimp'])) { $name_olimp = $_POST['name_olimp']; if ($name_olimp == '') { unset($name_olimp);} } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
-if (isset($_POST['location_olimp'])) { $location_olimp = $_POST['location_olimp']; if ($location_olimp == '') { unset($location_olimp);} }
+//if (isset($_POST['location_olimp'])) { $location_olimp = $_POST['location_olimp']; if ($location_olimp == '') { unset($location_olimp);} }
 if (isset($_POST['date_application'])) { $date_application = $_POST['date_application']; if ($date_application == '') { unset($date_application);} }
 if (isset($_POST['description_olimp'])) { $description_olimp = $_POST['description_olimp']; if ($description_olimp == '') { unset($description_olimp);} }
 if (isset($_POST['Org_olimp'])) { $Org_olimp = $_POST['Org_olimp']; if ($Org_olimp == '') { unset($Org_olimp);} }
@@ -26,13 +26,9 @@ $class_string="";
 $flag=true;
 $flag2=false;
 
-if($number_date==0){
-	$number_date=1;
-}
-
-for($i=1;$i<12;$i++){ //ваще не вкуриваю, для чего этот цикл
-	if($i==11&&$_POST['class_olimp'.$i]=="ON"){
-		if($flag==true&&$class_string!=""){
+for($i=1;$i<12;$i++){ //ваще не вкуриваю, для чего этот цикл // две недели спустя: ааааа, это он формирует классы, которые принимают участие!
+	if($i==11&&$_POST['class_olimp'.$i]=="ON"){				 // ааааааааааааааааа вот оно чо
+		if($flag==true&&$class_string!=""){					 // (c) Аля
 			$class_string=$class_string."-".($i);	
 			continue;
 		}
@@ -66,23 +62,55 @@ for($i=1;$i<12;$i++){ //ваще не вкуриваю, для чего этот
 //if (isset($_POST['select_subject'])) { $select_subject = $_POST['select_subject']; if ($select_subject == '') { unset($select_subject);} }
 // файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
 
-if (isset($_POST['number_date'])) { $number_date = $_POST['number_date']; if ($number_date == '') { unset($number_date);} }
-$id=mysql_insert_id();
-$date_time = '';
-
 $date_application = $_POST["year0"]."-".str_pad($_POST["month0"], 2, '0', STR_PAD_LEFT)."-".str_pad($_POST["day0"], 2, '0', STR_PAD_LEFT); //дата окончания приёма заявок
 
-//создаём строку с датами этапов
-for($i=1; $i<=$number_date; $i++){	
-	$time = $_POST["tm".$i];
-	$time1 = $_POST["1tm".$i];
-	$time2 = $_POST["2tm".$i];
+if($number_date==0){
+	$number_date=1;
+}
+if (isset($_POST['number_date'])) { $number_date = $_POST['number_date']; if ($number_date == '') { unset($number_date);} }
+$date_time = '';
+$stages = '';
+$location_olimp = '';
+$st_parent = '';
+
+if ($number_date == 1) {
+	$time = $_POST["tm1"];
+	$time1 = $_POST["1tm1"];
+	$time2 = $_POST["2tm1"];
 	$time3 = str_pad($time1, 2, '0', STR_PAD_LEFT).":".str_pad($time2, 2, '0', STR_PAD_LEFT);
 	
-	$date_time .= $_POST["year".$i]."-".str_pad($_POST["month".$i], 2, '0', STR_PAD_LEFT)."-".str_pad($_POST["day".$i], 2, '0', STR_PAD_LEFT)." ".$time3."!";	
-}	
+	$date_time .= $_POST["year1"]."-".str_pad($_POST["month1"], 2, '0', STR_PAD_LEFT)."-".str_pad($_POST["day1"], 2, '0', STR_PAD_LEFT)." ".$time3."!";
+	//$isChild = 0;
+	$stages = "0";
+	$location_olimp = $_POST["place1"];
+	
+	$id=mysql_insert_id();
+	mysql_query ("INSERT INTO olympics (name_olympiad, date, location,classes, terms,description, subject,professor_users_id, stages) VALUES('$name_olimp','$date_time','$location_olimp','$class_string', '$date_application','$description_olimp','$subject','$Org_olimp','$stages')",$db);
+	exit("<html><head><meta http-equiv='Refresh' content='0; URL=../index.php'></head></html>");
 
-mysql_query ("INSERT INTO olympics (name_olympiad, date, location,classes, terms,description, subject,professor_users_id) VALUES('$name_olimp','$date_time','$location_olimp','$class_string', '$date_application','$description_olimp','$subject','$Org_olimp')",$db);
-exit("<html><head><meta http-equiv='Refresh' content='0; URL=../index.php'></head></html>");
+} else {
+	for($i=1; $i<=$number_date; $i++){
+		$time = $_POST["tm".$i];
+		$time1 = $_POST["1tm".$i];
+		$time2 = $_POST["2tm".$i];
+		$time3 = str_pad($time1, 2, '0', STR_PAD_LEFT).":".str_pad($time2, 2, '0', STR_PAD_LEFT);
+		
+		$date_time = $_POST["year".$i]."-".str_pad($_POST["month".$i], 2, '0', STR_PAD_LEFT)."-".str_pad($_POST["day".$i], 2, '0', STR_PAD_LEFT)." ".$time3."!";
+		$location_olimp = $_POST["place".$i];
+		
+		$id=mysql_insert_id();
+		if ($i>1) { 
+			$stages = "0"; 
+			$st_parent .= $id."!";
+			$isChild = 1;
+		} else {
+			$parent_id = $id;
+		}
+		
+		mysql_query ("INSERT INTO olympics (name_olympiad, date, location,classes, terms,description, subject,professor_users_id, stages, IsChild) VALUES('$name_olimp','$date_time','$location_olimp','$class_string', '$date_application','$description_olimp','$subject','$Org_olimp','$stages','$isChild')",$db);
+	}
+	mysql_query("UPDATE olympics SET stages='$st_parent' WHERE id='$parent_id'");
+	exit("<html><head><meta http-equiv='Refresh' content='0; URL=../index.php'></head></html>");
+}
 
 ?>

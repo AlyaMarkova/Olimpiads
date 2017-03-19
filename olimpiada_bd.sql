c25cf831cdf9531f10eacfaf6ad617629a51ea2a
AlyaMarkova <alya.markova.3@gmail.com>
2017-03-19 13:33:58 +1000

    Обновлённая БД
    у поля phone значение по умолчанию NULL

--- olimpiada_bd.sql
+++ olimpiada_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 18 2017 г., 15:43
+-- Время создания: Мар 18 2017 г., 16:44
 -- Версия сервера: 5.5.53
 -- Версия PHP: 5.5.38
 
@@ -189,7 +189,7 @@ INSERT INTO `olympics` (`id`, `name_olympiad`, `Olympiad_status`, `date`, `descr
 CREATE TABLE `professor` (
   `users_id` int(11) NOT NULL,
   `Fio_professor` varchar(80) NOT NULL,
-  `phone` varchar(15) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `email` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -221,7 +221,7 @@ CREATE TABLE `schoolboy` (
   `school` varchar(200) CHARACTER SET cp1251 NOT NULL,
   `class` int(11) NOT NULL,
   `birthdate` date NOT NULL,
-  `phone` varchar(15) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `email` varchar(40) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `delivery` varchar(300) NOT NULL,
@@ -403,7 +403,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT для таблицы `olympics`
 --
 ALTER TABLE `olympics`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --

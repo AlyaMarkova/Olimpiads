c6ddc9028956c5ef6e152064d6d8d9a9c8337069
Саргылаана <hovalanshi@gmail.com>
2017-03-22 20:03:43 +1000

    вроде все

--- comments/class.comments.php
+++ comments/class.comments.php
@@ -374,7 +374,7 @@ function saveComments() {
 function formComment($replyid=0)
 	{
 	global $user;
-	if($_SESSION['login']!=""&&$_SESSION['activation']==1){
+	if($_SESSION['login']!=""&&$_SESSION['activation']>0){
 	if($this->login)
 		{
 		$pass_checked=md5($this->user['password'].$this->key);
